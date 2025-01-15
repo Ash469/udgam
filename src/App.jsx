9d86b1f02b49155a88a49ecd91706244a8fb3956
Ash469 <ashshandilya4@gmail.com>
2025-01-15 19:28:04 +0530

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import './styles/fonts.css';
 import AppRoutes from './Routes';
-
-
-
+import { VAKChat } from "vakchat";
+import "vakchat/dist/index.css";
 
 
 function App() {
@@ -13,6 +12,17 @@ function App() {
 
   return (
     <div className="App">
+      <VAKChat 
+            VAKFlowID="Mo2RDRGeqJUiyNXyYOLO" 
+            btnText="Udgam AI"
+            theme="light"
+            shade="indigo"
+            introMessage="Hello, I'm Udgam  AI assistant, here to help! How can I assist you today?"
+            defaultPopupSize="small"
+            emailRequired={false} 
+            contactRequired={true}
+            nameRequired={true} 
+        />
        <AppRoutes />
     </div>
   );
